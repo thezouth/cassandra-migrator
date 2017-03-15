2ab03883744f79ceb22f975f3c668ba08346d51e
Pitsanu Swangpheaw <pitsanu_s@hotmail.com>
2017-03-15 17:42:21 +0700

    add publish script

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
 from setuptools import setup
+import os
 
-with open('requirements.txt', 'r') as f:
-  lines = f.read().split('\n')
+package_requirements = os.path.join('cassandra_migrator.egg-info', 'requires.txt')
+local_requirements = 'requirements.txt'
+
+lines = []
+if os.path.exists(package_requirements):
+    with open(package_requirements, 'r') as f:
+      lines = f.read().split('\n')
+elif os.path.exists(local_requirements):
+    with open(local_requirements, 'r') as f:
+      lines = f.read().split('\n')
 
 requirements = [package for package in lines if package and not package.startswith('#')]
 
 setup(name='cassandra_migrator',
-      version='0.1.001',
+      version='0.1.4',
       description='cassanra migration tools',
       url='https://github.com/webscal3r/cassandra-migrator.git',
       author='Pitsanu Swangpheaw',
@@ -15,6 +24,5 @@ setup(name='cassandra_migrator',
       packages=['migrator'],
       zip_safe=False,
       install_requires=requirements,
-      scripts=['scripts/cmig'],
-      data_files=[('.', ['requirements.txt'])]
+      scripts=['scripts/cmig']
       )
